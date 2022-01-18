89c73124f4c76e0594dbfa9a5d034073ef3c8c57
Gaëtan Pascreau <62143619+GaetanPascreau@users.noreply.github.com>
2022-01-18 20:09:49 +0100

    added a test to check if an error message is created when the quantity is missing at the time of product creation

--- P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -151,5 +151,35 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
             // Assert
             Assert.Contains("The price must be greater than zero", modelErrors);
         }
+
+        [Fact]
+        public void CheckFieldValidationWithMissingQuantity()
+        {
+            // Arrange
+            // Mock all interfaces that are used in ProductService
+            Mock<ICart> mockCart = new Mock<ICart>();
+            Mock<IProductRepository> mockProductRepository = new Mock<IProductRepository>();
+            Mock<IOrderRepository> mockOrderRepository = new Mock<IOrderRepository>();
+            Mock<IStringLocalizer<ProductService>> mockStringLocalizer = new Mock<IStringLocalizer<ProductService>>();
+
+            var errorName = new LocalizedString("MissingQuantity", "Please enter a quantity");
+            mockStringLocalizer.Setup(ml => ml["MissingQuantity"]).Returns(errorName);
+            ProductService productService = new ProductService(mockCart.Object, mockProductRepository.Object, mockOrderRepository.Object, mockStringLocalizer.Object);
+            ProductViewModel product = new ProductViewModel
+            {
+                Id = 1,
+                Stock = null,
+                Price = "1",
+                Name = "z",
+                Description = "x",
+                Details = "y"
+            };
+
+            // Act
+            var modelErrors = productService.CheckProductModelErrors(product);
+
+            // Assert
+            Assert.Contains("Please enter a quantity", modelErrors);
+        }
     }
 }
